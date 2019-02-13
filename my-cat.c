017eb1cc3e0d64703043ca319104c56b13c175b0
Louisa Nyhus <20lcn1@cs.williams.edu>
2019-02-12 21:45:31 -0500

    first commit

--- my-cat.c
+++ my-cat.c
@@ -1,1 +1,20 @@
-// Students, replace with your code
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("usage: ./my-cat <filename>");
+  }
+  FILE* file = fopen(argv[1], "r");
+  if (file == NULL) {
+    printf("cannot open file\n");
+    exit(1);
+  }
+  char currentLine[2000];
+  while (fgets(currentLine, 2000, file) != NULL) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    printf("%s", currentLine);
+  }
+  fclose(file);
+}
+
+//ask bill about how to set buffer size!
--- my-grep.c
+++ my-grep.c
@@ -1,1 +1,37 @@
-// Students, replace with your code
+#include <stdio.h>
+#include <stdlib.h>
+
+void searchFile(char* f, char* w) {
+  FILE* file = fopen(f, "r");
+  //  char* word = w; //ok to have a char*???????
+  if (file == NULL) {
+    printf("cannot open file\n");
+    exit(1);
+  }
+  char currentLine[2000];
+  while (fgets(currentLine, 2000, file) != NULL) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    printf("%s", currentLine);
+  }
+  fclose(file);
+}
+
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("usage: ./my-grep <word> <filename(s)>");
+  }
+  //while (getopt(argc, argv, "") != -1) { //empty string?????????????
+  searchFile(argv[2], argv[1]);
+}
+
+
+
+
+
+
+
+
+//ask bill about how to set buffer size!
+//argc < vs !=???
+//what do we do with getopt if we aren't switching?
