45a5c10182240329bacb0b97e1ffcebe3b0a0096
Louisa Nyhus <20lcn1@cs.williams.edu>
2019-02-14 11:34:19 -0500

    added notes from office hours

--- my-cat.c
+++ my-cat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     printf("usage: ./my-cat <filename>");
@@ -10,11 +11,17 @@ int main(int argc, char* argv[]) {
     printf("cannot open file\n");
     exit(1);
   }
-  char currentLine[2000];
-  while (fgets(currentLine, 2000, file) != NULL) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  char currentLine[800];
+  //while (fgets(currentLine, 2000, file) != NULL) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  while (scanf("%800s", currentLine)) { 
     printf("%s", currentLine);
   }
   fclose(file);
 }
 
 //ask bill about how to set buffer size!
+
+
+//there should be a version of fgets that specifically sets the buffer size...same idea as what we have now but you can't have an overflow. the version of fgets or whatever will go until \n OR the specified size
+
+  //he also said that 800 is a good limit to use
