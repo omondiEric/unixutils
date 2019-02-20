ab2c7466b16455860d2c4f6d0a0257ee8e8c4596
Louisa Nyhus <20lcn1@cs.williams.edu>
2019-02-20 16:06:01 -0500

    hopefully the last submission!

--- my-unzip.c
+++ my-unzip.c
@@ -5,66 +5,47 @@
 #include <string.h>
 #include <stdint.h>
 
+/* my-unzip.c -- decodes a file by printing to stdout each character the
+ * number of times specified, as given by the file zipped by my-zip.c.
+ */
+
+//unzip the file
 void unzipFile(char* f) {
   FILE* file = fopen(f, "r");
-  if (file == NULL) {
+  if (file == NULL) { //don't work with bad files
     printf("my-unzip: cannot open file\n");
     exit(1);
   }
-  //char *currentLine = NULL;
-  //size_t len = 0;
-  //while (getline(&currentLine, &len, file) != -1) {
-    //int currNum = 0
-    //int currPos = 0;
-    //char currChar = currentLine[currPos]; //this is wasteful!!!!!!!!!
-    //while (currPos < strlen(currentLine)) {
-    /* while (currentLine[currPos] == currChar) {
-	currPos++;
-      }
-      printf("%d", currPos);*/
+  //print characters
   uint32_t currNum;
   char currChar;
+  //for every number-character pair
   while (fread(&currNum, sizeof(currNum), (size_t) 1, file) != 0) {
-    
-	  //fread(&currNum, sizeof(currNum), (size_t) 1, file); //is 4 right?????????
     fread(&currChar, sizeof(char), (size_t) 1, file);
-    //for (int i=currNum; i>0; i--) {
-      
-    // }
-    printf("%hu", currNum); //is it really a short int???????
-    printf("%c", currChar);
-    //printf("%PRIuint32", currNum);
-      /*char toPrint[100]; //ask bill!!!!!!!!!!!!!!!!!!!!!!!!!!
-      char intToChar = currPos + '0';
-      strcat(toPrint, &intToChar); //more efficient way???????????
-      strcat(toPrint, &currChar);
-      fwrite(&toPrint, (size_t) 5, (size_t) 1, stdout);*/
+
+    //print character as many times as needed
+    for (int i=currNum; i>0; i--) {
+      printf("%c", currChar);
+    }
   }
-  //}
-  //free(currentLine);
   fclose(file);
 }
 
 int main(int argc, char* argv[]) {
-  /*  if (argc < 2) {
-    printf("my-grep: searchterm [file ...]\n");
+  if (argc < 2) {
+    printf("my-unzip: file1 [file2 ...]\n");
     exit(1);
   }
-  if (argc < 3) {
-    char *currentLine = NULL;
-    size_t len = 0;
-    while (getline(&currentLine, &len, stdin) != -1) {
-    if (strstr(currentLine, argv[1]) != NULL) {
-      printf("%s", currentLine);
-    }
-    }
-    }*/
-  //else {
-    int fileCounter = 1;
+  int fileCounter = 1; //don't read in argv[0]
     while (fileCounter < argc) {
       unzipFile(argv[fileCounter]);
       fileCounter++;
     }
-    //}
     exit(0);
 }
+
+
+
+
+
+//PRIuint32??????????????
--- my-zip.c
+++ my-zip.c
@@ -5,53 +5,34 @@
 #include <string.h>
 #include <stdint.h>
 
-/* my-grep.c -- searches specified files for a given search term and prints
- * any lines containing the term.  If no search term given, no files given 
- * or invalid file given, exits with error.
+/* my-zip.c -- encodes a file by saving each character once (in ASCII),
+ * preceded by the number of sequential occurrences of that character.
  */
 
-
-//helper method that searches and prints one file
-/*void searchFile(char* f, char* w) {
-  FILE* file = fopen(f, "r");
-  if (file == NULL) {
-    printf("my-grep: cannot open file\n");
-    exit(1);
-  }
-  //check for term and print line if needed
-  char *currentLine = NULL;
-  size_t len = 0;
-  while (getline(&currentLine, &len, file) != -1) {
-    if (strstr(currentLine, w) != NULL) {
-      printf("%s", currentLine);
-    }
-  }
-  free(currentLine);
-  fclose(file);
-  }*/
-
+//zips the file
 void zipFile(char* f) {
   FILE* file = fopen(f, "r");
-  if (file == NULL) {
+  if (file == NULL) { //don't work with bad files
     printf("my-zip: cannot open file\n");
     exit(1);
   }
+
+  //for every line
   char *currentLine = NULL;
   size_t len = 0;
   while (getline(&currentLine, &len, file) != -1) {
-  //getline(&currentLine, &len, file);
+    //for every character in the line
     uint32_t linePos = 0;
     uint32_t currPos = 0;
-    char currChar = currentLine[0]; //this is wasteful!!!!!!!!!
-    while (linePos < strlen(currentLine)) { //or if it's super big????????????????
+    char currChar;
+    while (linePos < strlen(currentLine)) {
+      //for every sequence of characters
       currPos = 0;
       currChar = currentLine[linePos];
       while (currentLine[linePos+currPos] == currChar) {
 	currPos++;
       }
-      printf("%d", currPos);
-      linePos += currPos; //we need to make linepos skip one!!!!!!!!
-      //printf("%d", currPos);
+      linePos += currPos;
       fwrite(&currPos, sizeof(currPos), (size_t) 1, stdout); //better to just call 32 or sizeof?
       fwrite(&currChar, sizeof(currChar), (size_t) 1, stdout);    //is sizeof char ok????
     }
@@ -61,39 +42,23 @@ void zipFile(char* f) {
 }
 
 int main(int argc, char* argv[]) {
-  /*  if (argc < 2) {
-    printf("my-grep: searchterm [file ...]\n");
+  if (argc < 2) {
+    printf("my-zip: file1 [file2 ...]\n");
     exit(1);
   }
-  if (argc < 3) {
-    char *currentLine = NULL;
-    size_t len = 0;
-    while (getline(&currentLine, &len, stdin) != -1) {
-    if (strstr(currentLine, argv[1]) != NULL) {
-      printf("%s", currentLine);
-    }
-    }
-    }*/
-  //else {
-    int fileCounter = 1;
-    while (fileCounter < argc) {
-      zipFile(argv[fileCounter]);
-      fileCounter++;
-    }
-    //}
+  int fileCounter = 1; //don't read in argv[0]
+  while (fileCounter < argc) {
+    zipFile(argv[fileCounter]);
+    fileCounter++;
+  }
   exit(0);
 }
 
-
-  //in case we want to: getopt????????????
-
-//stdin and ctrl c
-//empty string specified as ""????????????????
 
 //from office hours: we need to loop through to ensure that if we have 2^32+1 characters it just prints 1!
 
 
 
-//what do we do with getopt if we aren't switching?
 
-//we don't care what myzip actually prints, just that we can unzip it
+//site says ints should be 4 bytes??????????????
+//do we care if newlines get dumped in with one each????????
